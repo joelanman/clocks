556e29dcc66bbc8339fa333b5358e8935a5d96c3
Joe Lanman <joelanman@gmail.com>
2013-04-28 22:18:42 +0100

    add and remove

--- main.js
+++ main.js
@@ -12,10 +12,10 @@ $(".dial").knob({
 
 
     'change' : function (v) {
-    	var hours = Math.floor(v/60),
-    		minutes = v%60;
-    	minutes = (minutes < 10) ? '0' + minutes : minutes;
-    	$('.clockDisplay').val(hours + ':' + minutes);
+		var hours = Math.floor(v/60),
+			minutes = v%60;
+			minutes = (minutes < 10) ? '0' + minutes : minutes;
+		$('.clockDisplay').val(hours + ':' + minutes);
     }
 });
 
@@ -24,21 +24,21 @@ $('.clockWrap').on('click touchstart', function(e){
 
 	var $this = $(this);
 
-	var hours 		= Number($this.find('.hours').text()),
-		minutes 	= Number($this.find('.minutes').text()),
-		totalMins 	= hours*60 + minutes,
-		location 	= $this.find('.location').text();
+	var hours		= Number($this.find('.hours').text()),
+		minutes		= Number($this.find('.minutes').text()),
+		totalMins	= hours*60 + minutes,
+		location	= $this.find('.location').text();
 
 	console.log(totalMins);
 
 	$(".dial").val(totalMins).trigger('change');
- 
- 	$('.clockDisplay').val(hours + ':' + minutes);
 
- 	$('.locationEdit input').val(location);
+	$('.clockDisplay').val(hours + ':' + minutes);
+
+	$('.locationEdit input').val(location);
 
 	$('#controlsWrap').fadeIn('fast');
-})
+});
 
 $('#controlsWrap').on('click touchstart', function(e){
 	e.preventDefault();
@@ -48,4 +48,37 @@ $('#controlsWrap').on('click touchstart', function(e){
 	}
 
 	$('#controlsWrap').fadeOut('fast');
+});
+
+$('#addClock').on('click touchstart', function(e){
+	e.preventDefault();
+
+	var newClock = $($('#clockTemplate').html().trim()).clone();
+
+	newClock.appendTo('#clocks');
+
+	drawClocks();
+});
+
+var drawClocks = function(){
+	var totalClocks = $('#clocks .clock').length;
+
+	if (totalClocks == 2){
+		$('.clockWrap').css({'width':'50%', 'height':'100%'});
+	}
+
+	if (totalClocks == 3){
+		$('.clockWrap').css({'width':'33%', 'height':'100%'});
+	}
+
+	if (totalClocks == 4){
+		$('.clockWrap').css({'width':'50%', 'height':'50%'});
+	}
+}
+
+$('body').on('click touchstart', '.clockWrap .remove', function(e){
+	e.preventDefault();
+	$(this).closest('.clockWrap').remove();
+	drawClocks();
+
 })
